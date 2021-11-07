7cc4fa7348356010a0d89eb6c826b71805ee4c7d
Clara-giraldo <clara.giraldocardona@gmail.com>
2021-11-06 23:03:30 -0500

    agregar card

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
+  
+  <div class="card">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
     
   );
 }
